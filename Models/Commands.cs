386722011c7fc2ea478a559a5459f8222376bb3d
marc pipa djole <pidjoma08@outlook.fr>
2020-06-10 12:58:49 -0400

    Added Command Model to API projet

---
+++ Models/Commands.cs
@@ -0,0 +1,10 @@
+namespace CommandAPI.Models
+{
+    public class CommandAPI
+    {
+        public int Id { get; set; }
+        public string howTo{get;set;}
+        public string Platform { get; set; }
+        public string CommandLine { get; set; }
+    }
+}
