0ee4e4ad5d1a2c8b368f81c859915c751708f462
marc pipa djole <pidjoma08@outlook.fr>
2020-06-18 23:15:54 -0400

    implementing Test1.1

---
+++ test/CommandAPI.Tests/CommandControllerTest.cs
@@ -0,0 +1,59 @@
+using System;
+using Xunit;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using CommandAPI.Controllers;
+using CommandAPI.Models;
+
+namespace CommandAPI.Tests
+{
+    public class CommandControllerTests:IDisposable
+    {
+        DbContextOptionsBuilder<CommandContext> optionsBuilder;
+        CommandContext dbContext;
+        CommandsController controller;
+        public CommandControllerTests()
+        {
+           optionsBuilder=new DbContextOptionsBuilder<CommandContext>();
+           optionsBuilder.UseInMemoryDatabase("UnitTestInMemBD");
+           dbContext=new CommandContext(optionsBuilder.Options);
+
+           controller=new CommandsController(dbContext);
+        }
+
+        public void Dispose()
+        {
+            optionsBuilder=null;
+            foreach (var cmd in dbContext.CommandItems)
+            {
+                dbContext.CommandItems.Remove(cmd);
+            }
+            dbContext.SaveChanges();
+            dbContext.Dispose();
+            controller=null;
+
+        }
+
+        //Action 1 Tests: get      /api/commands
+
+        //TEST 1.1 REQUEST OBJECTS WHEN NONE EXIST   -RETURN "NOTHING"
+        [Fact]
+        public void GetCommandItems_ReturnsZeroItems_WhenDBIsEmpty()
+        {
+            //ARRANGE
+            //DBContext
+
+            
+            //controller
+            //var controller=new CommandsController(dbContext);
+
+            //ACT
+            var result=controller.GetCommandItems();
+
+            //ASSERT
+            Assert.Empty(result.Value);
+        }             
+    }
+}
